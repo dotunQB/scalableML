038a801e5e3435ac05d65db454862a99d2f84f8c
dotunQB <oladotun.opasina@qauntumblack.com>
2023-01-22 09:14:31 -0500

    fixed feedback on model

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel, Field
 from ml.data import process_data
 from ml.model import inference, compute_model_metrics
 
-model_input = load(open('./model_slice.pkl','rb'))
+model_input = load(open('./model_out.pkl','rb'))
 encoder_out = load(open('./encoder_out.pkl','rb'))
 lb_out = load(open('./lb_out.pkl','rb'))
 cat_features = [
@@ -22,13 +22,19 @@ cat_features = [
 
 app = FastAPI()
 class CensusValue(BaseModel):
+    age: int
     workclass: str
+    fnlgt: int
     education: str
+    education_num: int = Field(alias="education-num")
     marital_status: str = Field(alias="marital-status")
     occupation: str
     relationship: str
     race: str
     sex: str
+    capital_gain: int = Field(alias="capital-gain")
+    capital_loss: int = Field(alias="capital-loss")
+    hours_per_week: int = Field(alias="hours-per-week")
     native_country: str = Field(alias="native-country")
     salary: str
 
@@ -36,13 +42,19 @@ class CensusValue(BaseModel):
         allow_population_by_field_name = True
         schema_extra = {
             "example": {
+                "age": 30,
                 "workclass": "Private",
+                "fnlgt": 123455,
                 "education": "Bachelors",
+                "education-num": 10,
                 "marital-status": "Never-married",
                 "occupation": "Adm-clerical",
                 "relationship": "Not-in-family",
                 "race": "White",
                 "sex": "Male",
+                "capital-gain": 10,
+                "capital-loss": 0,
+                "hours-per-week": 40,
                 "native-country": "Cuba",
                 "salary": "<=50K"
             }
@@ -56,6 +68,8 @@ async def root():
 
 @app.post("/predict_salary")
 def predict_salary(body: CensusValue):
+    print(list(body.dict().keys()))
+    print(list(body.dict().values()))
     values = pd.DataFrame(columns=list(body.dict().keys()))
     values = values.append(pd.Series(list(body.dict().values()) , index=list(body.dict().keys())),ignore_index=True)
     values.rename(columns={'marital_status': 'marital-status', 'native_country': 'native-country'}, inplace=True)
@@ -64,7 +78,9 @@ def predict_salary(body: CensusValue):
     )
     preds = inference(model_input, X_test)
     precision, recall, fbeta = compute_model_metrics(y_test, preds)
+    print(preds)
     return {
+        "prediction is ": int(preds),
         "precision is ": precision,
         "recall is ": recall,
         "fbeta is ": fbeta
--- train_model.py
+++ train_model.py
@@ -1,9 +1,11 @@
 # Script to train machine learning model.
 import pandas as pd
 import pickle
+import csv
 from sklearn.model_selection import train_test_split
 from ml.data import process_data
 from ml.model import train_model, inference, compute_model_metrics
+from joblib import load
 
 # load data from census csv
 data = pd.read_csv("./data/census.csv")
@@ -19,47 +21,70 @@ cat_features = [
     "native-country",
 ]
 
-def general_pipeline(data, cat_features, path_name):
+
+def slice_data_run(model_input, test_df, label, encoder, lb):
+    """ Function for calculating ml run on slices of the census dataset for salary.
+    model_input: Any
+            Picked model to be used for inference
+    test_df: pd.DataFrame
+            input dataframe to be utilized for run
+    label:
+        label class for data to be sliced
+    encoder:
+        encoder generated during training
+    lb:
+        binarizer generated during training
     """
-    Function to run the general modelling pipeline to avoid repetition
-    data : pd.DataFrame
-        Input Dataframe
-    cat_features: array
-        Categorical columns label for preprocessing
-    path_name: str
-        Output of where to save model output
-    """
-    train, test = train_test_split(data, test_size=0.20)
-    X_train, y_train, encoder, lb = process_data(
-        train, categorical_features=cat_features, label="salary", training=True
-    )
-    # Process the test data with the process_data function.
-    X_test, y_test, encoder, lb = process_data(
-        test, categorical_features=cat_features, label="salary", training=False,
-        encoder=encoder, lb=lb
-    )
-    # Train model and run evaluation on inference
-    model = train_model(X_train, y_train)
-    preds = inference(model, X_test)
-    precision, recall, fbeta = compute_model_metrics(y_test, preds)
-    print("saving output")
-    # Save model from path
-    pickle.dump(model, open(path_name, "wb"))
-    # Print out metrics
-    print("precision is: ", precision)
-    print("recall is: ", recall)
-    print("fbeta is: ", fbeta)
+    csvFile = open("slice_output.txt", "w", newline="", encoding='utf-8')
+    # csvFile.truncate()
+    csvWriter = csv.writer(csvFile)
+    # label = 'race'
+    for cls in test_df[label].unique():
+        test_slice = test_df[test_df[label] == cls]
+        X_test, y_test, encoder, lb = process_data(
+            test_slice, categorical_features=cat_features, label="salary", training=False,
+            encoder=encoder, lb=lb
+        )
+        preds = inference(model_input, X_test)
+        precision, recall, fbeta = compute_model_metrics(y_test, preds)
 
-    return encoder, lb
+        # Print out metrics
+        # print(f"Here is the model output for Race: {race}")
+        csvWriter.writerow([f"Here is the model output for {label}: {cls}"])
+        csvWriter.writerow(["Prediction for current run is: "])
+        csvWriter.writerow(preds)
+        csvWriter.writerow([f"Recall for current run is: "])
+        csvWriter.writerow([recall])
+        csvWriter.writerow([f"Fbeta for current run is: "])
+        csvWriter.writerow([fbeta])
+    csvFile.close()
 
 
-# run on general data without slices
-general_pipeline(data, cat_features, "model_out.pkl")
-
-### Run pipeline on slices of data, using categorical features
-slice_col=cat_features + ["salary"]
-sliced_data = data[slice_col]
-encoder, lb = general_pipeline(sliced_data, cat_features, "model_slice.pkl")
-
+train, test = train_test_split(data, test_size=0.20)
+X_train, y_train, encoder, lb = process_data(
+    train, categorical_features=cat_features, label="salary", training=True
+)
+# Process the test data with the process_data function.
+X_test, y_test, encoder, lb = process_data(
+    test, categorical_features=cat_features, label="salary", training=False,
+    encoder=encoder, lb=lb
+)
+# Train model and run evaluation on inference
+model = train_model(X_train, y_train)
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+print("saving output")
+# Save model from path
+pickle.dump(model, open("model_out.pkl", "wb"))
 pickle.dump(encoder, open("encoder_out.pkl", "wb"))
 pickle.dump(lb, open("lb_out.pkl", "wb"))
+# Print out metrics
+print("prediction for input is: ", preds)
+print("precision is: ", precision)
+print("recall is: ", recall)
+print("fbeta is: ", fbeta)
+
+# return encoder, lb
+model_input = load(open('./model_out.pkl', 'rb'))
+# run on slice of data
+slice_data_run(model_input=model_input, test_df=test, label='education', encoder=encoder, lb=lb)
