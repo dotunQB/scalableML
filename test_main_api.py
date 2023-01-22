17c04335677bea6209f412e51cf4aa939e8bb25d
dotunQB <oladotun.opasina@qauntumblack.com>
2023-01-21 22:15:36 -0500

    included test cases for api calls and passed sanity check

---
+++ test_main_api.py
@@ -0,0 +1,27 @@
+import json
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_get():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == {"message": "Welcome to Deploying ML Model to Cloud Application Exam with Census Data"}
+
+def test_post_data_sucess():
+    data ={"workclass":'Private', "education":'Bachelors', "marital_status":'Never-married',
+           "occupation":'Adm-clerical', "relationship":'Not-in-family',"race":'White',
+           "sex":'Male', "native_country":'Cuba', "salary":'<=50K'}
+    r = client.post("/predict_salary", data=json.dumps(data))
+    assert r.status_code == 200
+    assert r.json()
+
+def test_post_data_fail():
+    data ={"workclass": 0, "education":'Bachelors', "marital_status":'Never-married',
+           "occupation":'Adm-clerical', "relationship":'Not-in-family',"race":'White',
+           "sex":'Male', "native_country":'Cuba'}
+    r = client.post("/predict_salary", data=json.dumps(data))
+    assert r.status_code == 422
+    assert not r.json()
