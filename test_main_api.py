dc5ae0c5c7c820bd4e6ee41ecb5ae3f93a4bd7ec
dotunQB <oladotun.opasina@qauntumblack.com>
2023-01-22 16:32:28 -0500

    test case for greater and less than 50 k

--- test_main_api.py
+++ test_main_api.py
@@ -14,30 +14,30 @@ def test_get():
     assert r.json() == {"message": "Welcome to Deploying ML Model to Cloud Application Exam with Census Data"}
 
 def test_post_data_sucess_greater_50K():
-    data ={"age": 30,
-      "workclass": "Private",
-      "fnlgt": 123455,
-      "education": "Bachelors",
-      "education-num": 10,
-      "marital-status": "Never-married",
-      "occupation": "Adm-clerical",
-      "relationship": "Not-in-family",
+    data ={"age": 52,
+      "workclass": "Self-emp-not-inc",
+      "fnlgt": 209642,
+      "education": "HS-grad",
+      "education-num": 9,
+      "marital-status": "Married-civ-spouse",
+      "occupation": "Exec-managerial",
+      "relationship": "Wife",
       "race": "White",
-      "sex": "Male",
-      "capital-gain": 10,
+      "sex": "Female",
+      "capital-gain": 15024,
       "capital-loss": 0,
-      "hours-per-week": 40,
-      "native-country": "Cuba",
+      "hours-per-week": 45,
+      "native-country": "United-States",
       "salary": ">50K"
     }
     r = client.post("/predict_salary", data=json.dumps(data))
     assert r.status_code == 200
-    assert r.json() == {'prediction is ': '[0]', 'precision is ': 1.0, 'recall is ': 1.0, 'fbeta is ': 1.0}
+    assert r.json() == {'prediction is ': '[1]', 'precision is ': 0.0, 'recall is ': 1.0, 'fbeta is ': 0.0}
 
 def test_post_data_sucess_less_50k():
     data ={"age": 30,
       "workclass": "Private",
-      "fnlgt": 123455,
+      "fnlgt": 123456,
       "education": "Bachelors",
       "education-num": 10,
       "marital-status": "Never-married",
@@ -45,7 +45,7 @@ def test_post_data_sucess_less_50k():
       "relationship": "Not-in-family",
       "race": "White",
       "sex": "Male",
-      "capital-gain": 10,
+      "capital-gain": 0,
       "capital-loss": 0,
       "hours-per-week": 40,
       "native-country": "Cuba",
