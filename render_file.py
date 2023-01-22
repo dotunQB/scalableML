ac929e3509a919ecff51f90fe488ff1e9b4fa046
dotunQB <oladotun.opasina@qauntumblack.com>
2023-01-22 00:33:08 -0500

    added ability to request from a live deployment on render

--- render_file.py
+++ render_file.py
@@ -1,7 +1,21 @@
 """
 Test ml model on cloud deployed link through render
 """
+from requests.auth import HTTPBasicAuth
 import requests
+import json
+
+url = 'https://api_url'
+headers = {'Accept': 'application/json',
+           "Authorization": "Bearer rnd_iqq5CkM7BgAOqvR9jucjYUivERzl" }
 response = requests.get('https://render-scalableml.onrender.com/')
+print(response.status_code)
+print(response.json())
+
+data ={"workclass":'Private', "education":'Bachelors', "marital_status":'Never-married',
+           "occupation":'Adm-clerical', "relationship":'Not-in-family',"race":'White',
+           "sex":'Male', "native_country":'Cuba', "salary":'<=50K'}
+response = requests.post('https://render-scalableml.onrender.com/predict_salary',
+                         headers=headers, data=json.dumps(data))
 print(response.status_code)
 print(response.json())
